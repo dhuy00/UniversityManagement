36e70af7612d5a320631b3dfb5211b463a7b6984
dhuy00 <huyduc480@gmail.com>
2026-05-27 22:43:51 +0700

    handle for grant permsison procedure

--- script/sql/common_procedure.sql
+++ script/sql/common_procedure.sql
@@ -1,15 +1,51 @@
 CREATE OR REPLACE PROCEDURE Sp_Grant_Permission_To_User_Or_Role (
-    permission_type IN VARCHAR2,
-    tablename IN VARCHAR2,
-    target_name IN VARCHAR2,
-    is_grant_option IN NUMBER,
-    column_name_select IN VARCHAR2,--CHUOI CAC COLUMN DUOC NGAN CACH BOI DAU PHAY
-    column_name_update IN VARCHAR2
+    p_permission_type IN VARCHAR2,
+    p_tablename IN VARCHAR2,
+    p_target_name IN VARCHAR2,
+    p_is_grant_option IN NUMBER,
+    p_column_name_select IN VARCHAR2,
+    p_column_name_update IN VARCHAR2
 )
 AS
-    STRSQL VARCHAR(2000);
-    STRSQL2 VARCHAR(2000);
+    v_sql VARCHAR2;
+    v_view_name VARCHAR2;
+
+    v_table_name VARCHAR2;
+    v_target_name VARCHAR2;
 BEGIN
+    -- validate table name and target name
+    v_table_name := DBMS_ASSERT.SQL_OBJECT_NAME(UPPER(p_tablename));
+    v_target_name := DBMS_ASSERT.SIMPLE_SQL_NAME(UPPER(p_target_name));
+
+    IF (INSTR(UPPER(p_permission_type)), 'SELECT') > 0 THEN
+        IF p_column_name_select IS NOT NULL THEN
+            v_view_name := 'VIEW_SELECT_' || p_tablename || '_' || v_target_name;
+
+            -- drop view if exists, try to delet, if  get error, ignore it
+            BEGIN
+                EXECUTE IMMEDIATE 'DROP VIEW ' || v_view_name;
+            EXCEPTION
+                WHEN OTHERS THEN
+                    NULL;
+            END;
+
+            -- create view
+            v_sql := 'CREATE OR REPLACE VIEW' || v_view_name || ' AS SELECT ' || p_column_name_select
+            || ' FROM ' || v_table_name;
+
+            --grant select on view
+            v_sql := 'GRANT SELECT ON ' || v_view_name || ' TO ' || v_target_name;
+        ELSE
+            v_sql := 'GRANT SELECT ON ' || v_table_name || ' TO ' || v_target_name; 
+        END IF;
+
+        IF p_is_grant_option = 1 THEN
+            v_sql := v_sql || ' WITH GRANT OPTION';
+        END IF;
+
+        EXECUTE IMMEDIATE v_sql;
+    END IF;
+
     --NEU CHO PHEP SELECT TREN CAC COT CU THE
     IF column_name_select IS NOT NULL AND permission_type LIKE '%SELECT%' THEN
         STRSQL2 := '
